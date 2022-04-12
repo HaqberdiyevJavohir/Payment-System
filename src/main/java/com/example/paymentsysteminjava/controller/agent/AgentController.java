de61868779a3efe6c0385236ca428e8183a518b9
JavohirHaqberdiyev <haqberdiyev99@bk.ru>
2022-04-12 10:45:58 +0500

    All processes were moved from Kotlin to Java and some changes were made

--- src/main/java/com/example/paymentsysteminjava/controller/agent/AgentController.java
+++ src/main/java/com/example/paymentsysteminjava/controller/agent/AgentController.java
@@ -1,9 +1,14 @@
 package com.example.paymentsysteminjava.controller.agent;
 
 import com.example.paymentsysteminjava.dto.UserRegisterDto;
+<<<<<<< HEAD
 import com.example.paymentsysteminjava.dto.response.AgentRegisterDto;
 import com.example.paymentsysteminjava.service.jwt.JwtProvider;
 import com.example.paymentsysteminjava.service.user.UserService;
+=======
+import com.example.paymentsysteminjava.servise.jwt.JwtProvider;
+import com.example.paymentsysteminjava.servise.user.UserService;
+>>>>>>> 31f3465 (All processes were moved from Kotlin to Java and some changes were made)
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -26,6 +31,7 @@ public class AgentController {
 
     @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
     @PostMapping("/register")
+<<<<<<< HEAD
     public ResponseEntity<?> add(@RequestBody AgentRegisterDto agentRegisterDto) {
 
         return ResponseEntity.ok().body(userService.add(agentRegisterDto));
@@ -34,5 +40,24 @@ public class AgentController {
 
 
 
+=======
+    public ResponseEntity<?> add(@RequestBody UserRegisterDto userRegisterDto) {
+
+
+        Boolean add = userService.add(userRegisterDto);
+
+        if(add)
+            return ResponseEntity.ok().body("Created");
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Something is wrong");
+    }
+
+    @PostMapping("/api/check")
+    public ResponseEntity<?> check(
+            @RequestBody UserRegisterDto userRegisterDto
+    ){
+        return null;
+    }
+>>>>>>> 31f3465 (All processes were moved from Kotlin to Java and some changes were made)
 
 }
